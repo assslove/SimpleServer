02e5fe83d15ae7fb510676898728ea27b57d3649
assslove <houbin-12@163.com>
2014-04-06 23:56:37 +0800

    init project info

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  test 
+ *
+ *        Version:  1.0
+ *        Created:  04/06/2014 11:38:21 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:	genry , houbin-12@163.com
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) 
+{
+	printf("hello,world");
+	return 0;
+}
