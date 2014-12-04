e648a12d7221ebd8c634dee5e50c39e2a4c5f978
assslove <houbin-12@163.com>
2014-12-04 23:07:55 +0800

    add timer

---
+++ timer.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  timer.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年12月04日 23时07分39秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:	houbin , houbin-12@163.com
+ *   Organization:  XiaoNan, Inc. ShangHai CN. All rights reserved.
+ *
+ * =====================================================================================
+ */
+
+
