f1fadb9e2b144a428c64c598a6bb3222dfc2157e
houbin <houbin-12@163.com>
2015-01-04 11:22:48 +0800

    add some

---
+++ test_mcast.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test_mcast.c
+ *
+ *    Description:  test mcast
+ *
+ *        Version:  1.0
+ *        Created:  2015年01月04日 10时18分38秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:	houbin , houbin-12@163.com
+ *   Organization:  Houbin, Inc. ShangHai CN. All rights reserved.
+ *
+ * =====================================================================================
+ */
+
+
