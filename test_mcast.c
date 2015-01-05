7a7e88ab56ed43fcae3cfb9ede6318acc59c713f
houbin <houbin-12@163.com>
2015-01-05 18:37:25 +0800

    add some mcast

--- test_mcast.c
+++ test_mcast.c
@@ -19,7 +19,34 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
+
+int set_mcast_ttl(int fd, int ttl)
+{
+	return setsockopt(fd, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl));
+}
+
+int set_mcast_loop(int fd, int loop)
+{
+	return setsockopt(fd, IPPROTO_IP, IP_MULTICAST_LOOP, &loop, sizeof(loop));
+}
+
+int set_mcast_if(int fd, struct in_addr in)
+{
+	return setsockopt(fd, IPPROTO_IP, IP_MULTICAST_IF, &in, sizeof(struct in_addr));
+}
+
+int join_mcast(int fd, struct ip_mreq *req)
+{
+	return setsockopt(fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, req, sizeof(struct ip_mreq));
+}
+
+int leave_mcast(int fd, struct ip_mreq *req)
+{
+	return setsockopt(fd, IPPROTO_IP, IP_DROP_MEMBERSHIP, req, sizeof(struct ip_mreq));
+}
 
 int main(int argc, char* argv[])
 {
