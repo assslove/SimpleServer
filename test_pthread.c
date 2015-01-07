8a938bcb149495ec8cf76d9b1dcd04cd41e72700
houbin <houbin-12@163.com>
2015-01-07 17:30:29 +0800

    add some

--- test_pthread.c
+++ test_pthread.c
@@ -17,11 +17,41 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdint.h>
+#include <errno.h>
+#include <string.h>
 
+
+void* produce(void *arg)
+{
+	static int i = 0;
+	while (i != 10) {
+		sleep(1);
+		++i;
+		printf("child %u\n", i);
+	}
+
+	return NULL;
+}
 
 int main(int argc, char* argv[])
 {
+	pthread_t tid;
+
+	int ret = pthread_create(&tid, NULL, produce, NULL);
+	if (ret != 0) {
+		errno = ret;
+		printf("create pthread error! %s", strerror(errno));
+	}
+
+	printf("parent tid=%u\n", (uint32_t)tid);
+
+	pthread_join(pid, NULL);
+
+	sleep(20);
+
 	return 0;
 }
