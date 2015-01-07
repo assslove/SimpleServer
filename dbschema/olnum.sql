7b1190c9b8457758de403bfcd6a33448571fb0e4
houbin <houbin-12@163.com>
2015-01-07 15:32:15 +0800

    add some

--- dbschema/olnum.sql
+++ dbschema/olnum.sql
@@ -2,10 +2,10 @@
 create database stat;
 drop table if exists stat.t_ol_num_pid_2015;
 -- 表名为t_ol_num + pid + year
-create table stat.t_ol_num_pid_2015 (
+create table stat.t_ol_num_1_2015 (
 	sid int COMMENT 'server id',	
 	record_time int COMMENT 'record time', 
 	count int COMMENT 'count',  
 	primary key(sid, record_time)
-);
+) ENGINE=InnoDB default charset=utf8;
 
