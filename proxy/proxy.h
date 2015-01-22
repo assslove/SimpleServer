f1b664d04677005dd32f88aadbbd7916eadf6aed
houbin <houbin-12@163.com>
2015-01-22 15:07:03 +0800

    add some

--- proxy/proxy.h
+++ proxy/proxy.h
@@ -1,8 +1,17 @@
 #ifndef _PROXY_H_
 #define _PROXY_H_
 
-int handle_proxy(int fd, void *msg, int len);
 
-class FdCach
+#include <libnanc++/singleton.h>
+
+
+/* @brief 代理服务器实现逻辑
+ * @note 本服务器只适用于单角色多服或者单服
+ */
+class Proxy {
+
+};
+
+int handle_proxy(int fd, void *msg, int len);
 
 #endif 
