c0894f52239ca0ea1388d897b9c52c028cc1dd0a
houbin <houbin-12@163.com>
2015-01-26 15:09:57 +0800

    add data.cpp

---
+++ proxy/data.cpp
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  data.cpp
+ *
+ *    Description:  数据段
+ *
+ *        Version:  1.0
+ *        Created:  2015年01月26日 09时41分06秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:	houbin , houbin-12@163.com
+ *   Organization:  Houbin, Inc. ShangHai CN. All rights reserved.
+ *
+ * =====================================================================================
+ */
+
+
