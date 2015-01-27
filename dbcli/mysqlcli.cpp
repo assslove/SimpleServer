a5a9a53486c096bdfdab49ef4f1480a5c0d3c953
houbin <houbin-12@163.com>
2015-01-27 20:39:05 +0800

    add some

--- dbcli/mysqlcli.cpp
+++ dbcli/mysqlcli.cpp
@@ -22,10 +22,32 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <mysql.h>
-
+#include <string.h>
+#include <stdint.h>
 
 int main(int argc, char* argv[]) 
 {
-	printf("hello,world\n");
+	if (mysql_library_init(0, NULL, NULL)) {
+		fprintf(stderr, "could not init MySQL lib");
+		exit(1);
+	}
+	MYSQL *mysql = mysql_init(NULL);
+	mysql = mysql_real_connect(mysql, "127.0.0.1", "root", "8459328", NULL, 3306, NULL, 0);
+	if (!mysql) {
+		fprintf(stderr, "cannot not connect mysql");
+		exit(1);
+	}
+	mysql_select_db(mysql, "TEST");
+	mysql_autocommit(mysql, 0);
+	mysql_commit(mysql);
+	mysql_autocommit(mysql, 1);
+	char from[128] = {"select * from t_user"};
+	char to[1024];
+	uint32_t len = mysql_real_escape_string(mysql, to, from, strlen(from));
+	printf("len = %u\n from =%u", len, strlen(from));
+	mysql_real_query(mysql, to, 1024);
+	MYSQL_RES *res = mysql_store_result(mysql);
+	mysql_close(mysql);
+	mysql_library_end();
 	return 0;
 }
