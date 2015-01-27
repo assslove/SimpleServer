194df295050abebb65af1c4e99985aef6793c090
houbin <houbin-12@163.com>
2015-01-27 17:31:41 +0800

    test make

---
+++ test_make/add.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  add.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2015年01月20日 16时56分59秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:	houbin , houbin-12@163.com
+ *   Organization:  Houbin, Inc. ShangHai CN. All rights reserved.
+ *
+ * =====================================================================================
+ */
+
+int add(int a, int b)
+{
+	return a + b;
+}
---
+++ test_make/main.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2015年01月20日 17时09分21秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:	houbin , houbin-12@163.com
+ *   Organization:  Houbin, Inc. ShangHai CN. All rights reserved.
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+#include "add.h"
+
+int main(int argc, char* argv[])
+{
+	int c = add(1, 2);
+	return 0;
+}
