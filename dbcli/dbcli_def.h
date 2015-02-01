62882b0e7ec015c0de5728f59ccc7c426a0d9f41
assslove <houbin-12@163.com>
2015-02-01 22:09:18 +0800

    add some

--- dbcli/dbcli_def.h
+++ dbcli/dbcli_def.h
@@ -1,6 +1,10 @@
 #ifndef _DBCLI_DEF_H
 #define _DBCLI_DEF_H
 
+#include <stdlib.h>
+#include <stdint.h>
+#include <mysql.h>
+
 /* @brief 错误码内部定义 1-1000 预留，应用程序不可使用
  */
 enum DB_ERROR_CODE {
@@ -9,14 +13,62 @@ enum DB_ERROR_CODE {
 	DB_ERR_STORE_RES = 2,	//存储结果失败
 };
 
-inline safe_atoi(const char *str) 
+/* @brief str to int
+ */
+inline int safe_atoi(const char *str) 
 {
 	return (str != NULL) ? atoi(str) : 0;
 }
 
-inline safe_atol(const char *str) 
+/* @brief str to uint64_t
+ */
+inline uint64_t safe_atol(const char *str) 
 {
 	return (str != NULL) ? atoll(str) : 0;
 }
+
+//开始单个查询开始
+#define QUERY_ONE_BEGIN(nofind_err) { \
+	MYSQL_RES *res; \
+	MYSQL_ROW row;\
+	int count, ret;\
+	ret = this->execQuerySql(&res, &count);\
+	if (ret == 0) {\
+		if (count != 1) {\
+			mysql_free_result(res);\
+			DEBUG(0, "no record exist [%u]", nofind_err);\
+			return no_finderr;\
+		} else { \
+			row = mysql_fetch_row(res);	\
+			int idx_ = -1;\
+
+//开始单个查询结束
+#define QUERY_ONE_END() \
+			mysql_free_result(res); \
+			return 0;\
+		}\
+	} else {\
+		return DB_ERR; \
+	}
+
+//下一行
+#define  NEXT_COL	(row[++idx_])
+
+//cpy int 
+#define INT_NEXT_COL(val)  (val)=safe_atoi(NEXT_COL)
+
+//cpy int64
+#define INT64_NEXT_COL(val)  (val)=safe_atol(NEXT_COL)
+
+//拷贝字符串
+#define STR_NEXT_COL(val, max) \
+	++idx_;\
+	mysql_fetch_lengths(res); \
+	if (res->lengths[idx_] < max) {\
+		memcpy(val, row[idx_], res->lengths[idx_]);\
+	} else {\
+		memcpy(val, row[idx_], max);\
+	}
+
 
 #endif
